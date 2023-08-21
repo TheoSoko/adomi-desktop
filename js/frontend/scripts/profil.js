75aa06b0813f1442b78a797e914a312be6c7334c
konomoha <konomoha@hotmail.fr>
2023-08-21 10:48:17 +0200

    maj

--- js/frontend/scripts/profil.js
+++ js/frontend/scripts/profil.js
@@ -4,7 +4,7 @@ let userCardText = "";
 window.submitForm.getUserProfile().then((profile) => {
     for (let data in profile) {
         if (data !== 'id') {
-            userCardText += `<tr class='p-5'><td><b>${data}</b></td><td>${profile[data]}</td></tr>`;
+            userCardText += `<tr><td><b>${data}</b></td><td>${profile[data]}</td></tr>`;
         }
     }
     userCard.innerHTML = userCardText;
